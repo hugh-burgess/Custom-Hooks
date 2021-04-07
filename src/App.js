b584d5a217534a6e2c945a45682e7f35bb30a934
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-04-07 17:56:08 +0200

    5 done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   // Add states and eventHandlers here
 
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userAge, setUserAge] = useState();
+  const [userEmail, setUserEmail] = useState("");
+  const [isChecked, setIsChecked] = useState(false);
+  function handleChange(event) {
+    const { value, name, checked } = event.target;
+
+    if (name === "firstName") {
+      setFirstName(value);
+    } else if (name === "lastName") {
+      setLastName(value);
+    } else if (name === "age") {
+      setUserAge(value);
+    } else if (name === "email") {
+      setUserEmail(value);
+    } else if (name === "newsletter") {
+      setIsChecked(checked);
+    }
+    console.log(handleChange());
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
+    const form = event.target;
     console.log("submitted");
+    const userData = {
+      firstName,
+      lastName,
+      userAge,
+      userEmail,
+      isChecked,
+    };
+    console.log(userData);
+    form.reset();
   }
 
   return (
@@ -14,18 +47,50 @@ function App() {
       <h2>Enter user credentials below:</h2>
       <form onSubmit={handleSubmit}>
         <label htmlFor="firstName">Name:</label>
-        <input type="text" name="firstName" />
+        <input
+          value={firstName}
+          onChange={handleChange}
+          type="text"
+          name="firstName"
+          required
+        />
         <label htmlFor="lastName">Surname:</label>
-        <input type="text" name="lastName" />
+        <input
+          value={lastName}
+          onChange={handleChange}
+          type="text"
+          name="lastName"
+          required
+        />
         <label htmlFor="age">Age:</label>
-        <input type="number" name="age" />
+        <input
+          value={userAge}
+          onChange={handleChange}
+          type="number"
+          name="age"
+          required
+        />
         <label htmlFor="email">Email:</label>
-        <input type="email" name="email" />
+        <input
+          value={userEmail}
+          onChange={handleChange}
+          type="email"
+          name="email"
+          required
+        />
         <div className="newsletter">
-          <input type="checkbox" name="newsletter" />
+          <input
+            checked={isChecked}
+            onChange={handleChange}
+            type="checkbox"
+            name="newsletter"
+            required
+          />
           <label htmlFor="newsletter">
             Yes, I would like many more emails!
           </label>
+          <br />
+          <button type="submit">Submit</button>
         </div>
       </form>
     </div>
